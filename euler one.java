fc3f2afb866f0f6e85f61e2838f1bedf645a5b5d
FearlessIIX <84805129+FearlessIIX@users.noreply.github.com>
2021-07-05 19:36:58 -0400

    created euler one in java

---
+++ euler one.java
@@ -0,0 +1,8 @@
+//sum of all multiples of 3 and 5 below 1000
+int sum = 0;
+for (int x = 1; x < 1000; x++) {
+    if (x % 3 == 0 || x % 5 == 0) {
+        sum += x;
+    }
+}
+System.out.print(sum);
