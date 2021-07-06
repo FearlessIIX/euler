b22a2fb659a3ea087ba90961f5f99212fe157b30
FearlessIIX <84805129+FearlessIIX@users.noreply.github.com>
2021-07-05 20:20:01 -0400

    eulers in java

--- java/EulerSeven.java
+++ java/EulerSeven.java
@@ -5,7 +5,7 @@
 
 public class EulerSeven {
     public static void main(String[] args) {
-        //starting at zero, finds the 10,001st prime number
+        //starting at two, finds the 10,001st prime number
         int stored = 0; int count = 0; int num = 2;
         while (count < 10_001) {
             if (Euler.Three.isPrime(num)) {
